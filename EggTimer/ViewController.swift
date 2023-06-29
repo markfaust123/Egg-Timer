37b650fbba1b4e74e5af318fa5ef80223e4eb530
Mark Faust <markfaust123@gmail.com>
2023-06-29 13:18:04 -0400

    Countdown now takes place in debug console!

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,23 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    // DICTIONARY
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle
+        // can confidently unwrap because know spellings to be correct and not going to contain nil values
+        let hardness = sender.currentTitle!
+        let seconds = eggTimes[hardness]!
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        default:
-            print(hardTime)
-        }
+        var secondsRemaining = seconds * 60
         
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+                if secondsRemaining > 0 {
+                    print ("\(secondsRemaining) seconds.")
+                    secondsRemaining -= 1
+                } else {
+                    Timer.invalidate()
+                }
+            }
+
+//        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: seconds, repeats: true)
+
+        
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        default:
+//            print(hardTime)
+//        }
+        
+//        Optional Strings are automatically unwrapped when used in comparison?
 //        if hardness == "Soft" {
 //            print(softTime)
 //        } else if hardness == "Medium" {
@@ -35,6 +51,15 @@ class ViewController: UIViewController {
 //            print(hardTime)
 //        }
         
+    }
+    
+    @objc func updateCounter(time: Int) {
+        var time = time
+        //example functionality
+        if time > 0 {
+            print("\(time) seconds.")
+            time -= 1
+        }
     }
     
 }
